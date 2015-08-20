34c404935ba8224da703b7c3f6a0e5567c6306a0
Michael Sprague <msprague@clearblade.com>
2015-08-20 12:24:22 -0400

    finish up Todo unit tests

--- app/app.tests.js
+++ app/app.tests.js
@@ -2,6 +2,3 @@ import 'babel-core/polyfill';
 
 let context = require.context('.', true, /-test\.jsx?$/);
 context.keys().forEach(context);
-
-// POC for running specific unit test - need to talk through this with Tom
-//require('./components/Header/__tests__/Header-test');
