6138f4f126ba6e722ff5b704846de1bd4dbacabb
tompryor <tom@dvia.com>
2015-08-23 20:31:44 -0400

    fix typo in "FlyOutMenuElement" test.

--- app/components/FlyOutMenu/__tests__/FlyOutMenu-test.jsx
+++ app/components/FlyOutMenu/__tests__/FlyOutMenu-test.jsx
@@ -5,13 +5,13 @@ import { assert } from 'chai';
 let { TestUtils } = React.addons;
 
 describe('FlyOutMenu', () => {
-    it('Should have the correct css class', () => {
+    it('FlyOutMenu element should have the correct css class', () => {
 
         let flyOutMenu = TestUtils.renderIntoDocument(
             <FlyOutMenu title="Testing"/>
         );
         let flyOutMenuElement = React.findDOMNode(flyOutMenu);
-        assert.equal(flyOutMenuElement.className, 'FlyOutMenu', 'FlyOutMenu class not applied to element');
+        assert.equal(flyOutMenuElement.className, 'FlyOutMenu', 'FlyOutMenu class not applied to FlyOutMenu element');
 
     });
 
